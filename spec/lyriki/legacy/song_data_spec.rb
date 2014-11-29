4c509c60f839df77e49369bc02f69d068634bc6b
Alexander <alxndr+github@gmail.com>
2014-11-29 14:55:03 -0800

    tidying

--- spec/lyriki/legacy/song_data_spec.rb
+++ spec/lyriki/legacy/song_data_spec.rb
@@ -4,26 +4,24 @@ describe Lyriki::Legacy::SongData do
 
   subject { Lyriki::Legacy::SongData.new artist: "fz", song: "inca roads" }
 
-  it "should exist" do
-    expect(subject).to be_truthy
+  it { is_expected.to be_truthy }
+
+  describe "#response_data" do
+    it { is_expected.to respond_to :response_data }
+    it "returns data"
   end
 
   describe "initialize" do
     describe "missing artist" do
       it "should throw" do
-        expect{ Lyriki::Legacy::SongData.new song: "inca roads" }.to raise_error(ArgumentError)
+        expect { Lyriki::Legacy::SongData.new song: "inca roads" }.to raise_error(ArgumentError)
       end
     end
     describe "missing song" do
       it "should throw" do
-        expect{ Lyriki::Legacy::SongData.new artist: "fz" }.to raise_error(ArgumentError)
+        expect { Lyriki::Legacy::SongData.new artist: "fz" }.to raise_error(ArgumentError)
       end
     end
-  end
-
-  describe "#response_data" do
-    it { is_expected.to respond_to :response_data }
-    it "returns data"
   end
 
 end
