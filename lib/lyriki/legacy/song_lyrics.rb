5a0e7ae7111c2b28cfb7e4a69940b781392846a4
Alexander <alxndr+github@gmail.com>
2014-11-29 16:35:36 -0800

    use the get helper

--- lib/lyriki/legacy/song_lyrics.rb
+++ lib/lyriki/legacy/song_lyrics.rb
@@ -1,18 +1,21 @@
 require "json"
 require "nokogiri"
+require "web_helpers"
 
 module Lyriki
   module Legacy
     class SongLyrics
 
+      include WebHelpers
+
       def initialize(**args)
         raise ArgumentError unless args[:artist] && args[:song]
         song_data = SongData.new(args).response_data
-        @data = Net::HTTP.get(URI(song_data['url']))
+        @data = get(song_data["url"])
       end
 
       def response_data
-        Nokogiri::HTML(@data).css('div.lyricbox/text()').map(&:text)
+        Nokogiri::HTML(@data).css("div.lyricbox/text()").map(&:text)
       end
 
     end
