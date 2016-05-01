f3281853c410e5d9af4ca23dbd9d907a339f14e0
Alexander <alxndr+github@gmail.com>
2016-05-01 11:06:09 -0700

    raise if trying to fetch lyrics from edit page

--- lib/web_helpers.rb
+++ lib/web_helpers.rb
@@ -5,6 +5,8 @@ module WebHelpers
   def get(url, limit=10)
     raise ArgumentError, "get() redirects too deep" if limit < 1
 
+    raise EditUrlError, "shouldn't be trying to edit... #{url}" if url =~ /[?&]action=edit\b/
+
     response = Net::HTTP.get_response(URI(url))
     case response
     when Net::HTTPSuccess     then response.body
@@ -18,3 +20,5 @@ module WebHelpers
   end
 
 end
+
+class EditUrlError < StandardError; end
